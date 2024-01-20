cf6a87118dab59721858520d42159b8d9ba51e64
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-20 16:20:49 +0545

    created profile card

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,8 +7,8 @@ import { BsFillSave2Fill } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
-    <div className=" md:px-5 w-[15%] md:w-[20%] pt-5 border-r-2 border-gray-300 ">
-      <div className="flex flex-col gap-4">
+    <div className=" md:px-5 w-[15%] md:w-[20%] pt-5 border-r-2 border-gray-200 ">
+      <div className="flex flex-col gap-4 pb-4 border-b-2 border-gray-200">
         <div className="flex items-center gap-4 px-4 py-4 transition-colors duration-300 rounded-xl hover:bg-red-300 hover:cursor-pointer">
           <AiFillHome className="size-8 md:size-6" color="grey" />
           <p className="hidden text-xl text-gray-600 md:block">Home</p>
